07f9428b0e531310b51f49147902361cb4e05997
João Marcos Aires Duarte <jmarcos_duarte@hotmail.com>
2020-04-21 01:33:31 -0400

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author JM
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.printf("A sua Média é: %.1f \n", m);*/
+       
+       /*int numero = 5;
+       numero++;
+       numero--;
+       ++numero;
+       --numero;
+       System.out.println("O Valor do Número é: " + numero);*/
+       /*int valor = 5 + numero++;
+       System.out.println("O Valor é igual a: " + valor);
+       System.out.println("O Valor do Número é: " + numero);*/
+       /*int valor = 5 + ++numero;
+       System.out.println("O Valor é igual a: " + valor);
+       System.out.println("O Valor do Número é: " + numero);*/
+       /*int numero = 10;
+       int valor = 4 + numero--;
+       System.out.println("O Valor é igual a: " + valor);
+       System.out.println("O Valor do Número é: " + numero);
+       int valor = 4 + --numero;
+       System.out.println("O Valor é igual a: " + valor);
+       System.out.println("O Valor do Número é: " + numero);*/
+       //Valores Inteiros
+      /* int x = 4;
+       x += 2;
+       System.out.println("O Valor de X é igual a: " + x);
+       x -= 2;
+       System.out.println("O Valor de X é igual a: " + x);
+       x *= 2;
+       System.out.println("O Valor de X é igual a: " + x);
+       //Valores Reais
+       double y = 4;
+       y /= 2;
+       System.out.printf("O Valor de Y é igual a: %.1f \n", y);
+       y %= 2;
+       System.out.printf("O Resto da Divisão de Y é igual a: %.1f \n: ", y);*/
+      /*
+      float valor = 8.3f;
+      //Arrendondamento para baixo
+      int arrendondar = (int) Math.floor(valor);
+      System.out.println("O Valor Arrendondado é: " + arrendondar);
+      //Arrendondamento para cima
+      int arrendondar2 = (int) Math.ceil(valor);
+      System.out.println("O Valor Arrendondado é: " + arrendondar2);
+      //Arrendomento Aritmético
+      //valor = 8.5f;
+       int arrendondar3 = (int) Math.round(valor);
+      System.out.println("O Valor Arrendondado é: " + arrendondar3);*/
+      
+      /*double aleatorio = Math.random();
+      System.out.println("Os Valores Gerados Aleatoriamente são: " + aleatorio);*/
+      
+      /*int aleatorio = (int) (5 + Math.random() * (10-5));
+      System.out.println("Os Valores Gerados Aleatoriamente são: " + aleatorio);*/
+      
+      int aleatorio = (int) (15 + Math.random() * (50-15));
+      System.out.println("Os Valores Gerados Aleatoriamente são: " + aleatorio);
+        
+      
+      
+        
+    }
+    
+}
